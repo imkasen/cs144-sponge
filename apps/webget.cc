7759f7483fac18867becc229d62ad053617c0d28
Kasen <18170166+imkasen@users.noreply.github.com>
2022-10-16 15:56:50 +0800

    lab0

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,3 +1,4 @@
+#include "address.hh"
 #include "socket.hh"
 #include "util.hh"
 
@@ -13,12 +14,32 @@ void get_URL(const string &host, const string &path) {
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
 
+    Address addr = Address(host, "http");
+    TCPSocket sock;
+    sock.connect(addr);
+
+    sock.write("GET " + path + " HTTP/1.1\r\n");
+    sock.write("Host: " + host + "\r\n");
+    sock.write("Connection: close\r\n");
+    sock.write("\r\n");
+
+    // If you don’t shut down your outgoing byte stream,
+    // the server will wait around for a while for you to send additional requests
+    // and won’t end its outgoing byte stream either.
+    sock.shutdown(SHUT_WR);
+
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    while (!sock.eof()) {
+        cout << sock.read();
+    }
+
+    sock.close();
+
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,5 +1,8 @@
 #include "byte_stream.hh"
 
+#include <algorithm>
+#include <string>
+
 // Dummy implementation of a flow-controlled in-memory byte stream.
 
 // For Lab 0, please replace with a real implementation that passes the
@@ -7,47 +10,64 @@
 
 // You will need to add private members to the class declaration in `byte_stream.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
-ByteStream::ByteStream(const size_t capacity) { DUMMY_CODE(capacity); }
+ByteStream::ByteStream(const size_t capacity)
+    : _buffer(), _capacity(capacity), _written_size(0), _read_size(0), _is_end_input(false) {}
 
 size_t ByteStream::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    if (input_ended()) {
+        return 0;
+    }
+
+    size_t write_size = std::min(data.length(), remaining_capacity());
+    _written_size += write_size;
+
+    // 如果 write_size < data.length()，多余的部分会被丢弃
+    for (size_t i = 0; i < write_size; ++i) {
+        _buffer.push_back(data[i]);
+    }
+
+    return write_size;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    DUMMY_CODE(len);
-    return {};
+    size_t peek_size = std::min(len, buffer_size());
+    return std::string(_buffer.begin(), _buffer.begin() + peek_size);
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
-void ByteStream::pop_output(const size_t len) { DUMMY_CODE(len); }
+void ByteStream::pop_output(const size_t len) {
+    size_t pop_size = std::min(len, buffer_size());
+    _read_size += pop_size;
+    while (pop_size--) {
+        _buffer.pop_front();
+    }
+}
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
 //! \param[in] len bytes will be popped and returned
 //! \returns a string
 std::string ByteStream::read(const size_t len) {
-    DUMMY_CODE(len);
-    return {};
+    std::string data = peek_output(len);
+    pop_output(len);
+    return data;
 }
 
-void ByteStream::end_input() {}
+void ByteStream::end_input() { _is_end_input = true; }
 
-bool ByteStream::input_ended() const { return {}; }
+bool ByteStream::input_ended() const { return _is_end_input; }
 
-size_t ByteStream::buffer_size() const { return {}; }
+size_t ByteStream::buffer_size() const { return _buffer.size(); }
 
-bool ByteStream::buffer_empty() const { return {}; }
+bool ByteStream::buffer_empty() const { return _buffer.empty(); }
 
-bool ByteStream::eof() const { return false; }
+// eof 成立的条件是 writer 不再写入，同时 reader 读取完全部数据
+bool ByteStream::eof() const { return input_ended() && buffer_empty(); }
 
-size_t ByteStream::bytes_written() const { return {}; }
+size_t ByteStream::bytes_written() const { return _written_size; }
 
-size_t ByteStream::bytes_read() const { return {}; }
+size_t ByteStream::bytes_read() const { return _read_size; }
 
-size_t ByteStream::remaining_capacity() const { return {}; }
+size_t ByteStream::remaining_capacity() const { return _capacity - buffer_size(); }
