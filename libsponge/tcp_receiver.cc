c3df77a4aed9987da5b1f47368dc5043cd11cc2c
Kasen <18170166+imkasen@users.noreply.github.com>
2022-11-14 17:26:46 +0800

    TCP receiver

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -5,15 +5,37 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader &header = seg.header();
+
+    if (!_syn_flag) {
+        if (!header.syn) {
+            return;  // 直接丢弃
+        }
+        _syn_flag = header.syn;
+        _isn = header.seqno;
+    }
+    // SYN_RECV 状态
+    uint64_t checkpoint = header.syn ? 0 : stream_out().bytes_written() - 1;  // the index of last reassembled byte
+    uint64_t abs_seqno = unwrap(header.seqno, _isn, checkpoint);
+    uint64_t stream_idx = header.syn ? 0 : abs_seqno - 1;  // abs seqno 换算到 stream index，留意 SYN 为 true 的情况
+    _reassembler.push_substring(seg.payload().copy(), stream_idx, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    // 非 LISTEN 状态
+    if (!_syn_flag) {
+        return std::nullopt;
+    }
+    // LISTEN 状态
+    uint64_t abs_ackno = stream_out().bytes_written() + 1;  // 加上 SYN flag 的长度
+    // FIN_RECV 状态
+    if (stream_out().input_ended()) {
+        abs_ackno += 1;  // 加上 FIN flag 的长度
+    }
+    return wrap(abs_ackno, _isn);
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - stream_out().buffer_size(); }
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,12 +20,15 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    bool _syn_flag;
+    WrappingInt32 _isn;
+
   public:
     //! \brief Construct a TCP receiver
     //!
     //! \param capacity the maximum number of bytes that the receiver will
     //!                 store in its buffers at any give time.
-    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity) {}
+    TCPReceiver(const size_t capacity) : _reassembler(capacity), _capacity(capacity), _syn_flag(false), _isn(0) {}
 
     //! \name Accessors to provide feedback to the remote TCPSender
     //!@{
